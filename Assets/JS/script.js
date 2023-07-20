e74d9aa44aba5c75af79476178011a43bfce8bbd
mark0698 <markokeeffe98@gmail.com>
2023-07-20 10:36:07 +0000

    fixing typos to link the written logic to the HTML

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,35 +1,45 @@
-const userChoice = document.querySelector ('#userChoice')
-const computerChoice = document.querySelector ('#computerChoice')
-const winnerIs = document.querySelector ('#winnerIs')
+const userChoice = document.querySelector('#userChoice');
+const computerChoice = document.querySelector('#computerChoice');
+const winnerIs = document.querySelector('#winnerIs');
 
-const choicebutton = document.querySelectorAll ('.choicebutton')
+const choicebutton = document.querySelectorAll('.choicebutton');
 
-let User;
-let Player;
+let player;
+let computer;
 let result;
 
-choicebutton.forEach (button => button.addEventListener('click', () => {
+choicebutton.forEach(button => button.addEventListener('click', () => {
     player = button.textContent;
-    computersGo = ();
-    userChoice.textContent = `Player: ${Player}`;
-    computerChoice.textContent = `Computer: ${Computer}`;
-    winnerIs.textContent = checkwinner()
+    computersGo();
+    userChoice.textContent = `Player: ${player}`;
+    computerChoice.textContent = `Computer: ${computer}`;
+    winnerIs.textContent = checkwinner();
+}));
 
+function computersGo() {
+    const random = Math.floor(Math.random() * 3) + 1;
 
-}))
+    switch (random) {
+        case 1:
+            computer = 'Rock';
+            break;
+        case 2:
+            computer = 'Paper';
+            break;
+        case 3:
+            computer = 'Scissors';
+            break;
+    }
+}
 
-function computersGo (){
-    const random = math.floor(math.random() *3 )+1;
-
-    switch (random){
-            case 1: 
-        computer = 'btn btn--big btn--rock';
-        break ; 
-            case 2:
-            computer = 'btn btn--big btn--paper'
-        break ;
-            case 3: 
-        computer = 'btn btn--big btn--scissors'
-
+function checkwinner() {
+    if (player === computer) {
+        return "Draw!";
+    } else if (computer === 'Rock') {
+        return (player === 'Paper') ? 'You Win!' : 'You Lose!';
+    } else if (computer === 'Paper') {
+        return (player === 'Scissors') ? 'You Win!' : 'You Lose!';
+    } else if (computer === 'Scissors') {
+        return (player === 'Rock') ? 'You Win!' : 'You Lose!';
     }
 }
