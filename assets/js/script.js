9782deab932f6ae68534cb9cd6c182f815c793f1
mark0698 <markokeeffe98@gmail.com>
2023-07-29 09:02:02 +0000

    add logic to start game and rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,36 @@
-/* declaring of all variables used throughtout */
-const userChoice = document.querySelector('#userChoice');
-const computerChoiceElement = document.querySelector('#computerChoice');
-const winnerIs = document.querySelector('#winnerIs');
-const playerScoreElement = document.querySelector('#userScore');
-const computerScoreElement = document.querySelector('#computerScore');
-const drawScoreElement = document.querySelector('#drawScore');
-const choicebutton = document.querySelectorAll('.choicebutton');
+/* declaring of all variables used throughout */
+const userChoice = document.querySelector("#userChoice");
+const computerChoiceElement = document.querySelector("#computerChoice");
+const winnerIs = document.querySelector("#winnerIs");
+const playerScoreElement = document.querySelector("#userScore");
+const computerScoreElement = document.querySelector("#computerScore");
+const drawScoreElement = document.querySelector("#drawScore");
+const choiceButton = document.querySelectorAll(".choice-button");
+
+const startButton = document.getElementById("start-game");
+const readRulesButton = document.getElementById("read-rules");
+const whateverButton = document.getElementById("whatever");
+
+const gameScreen = document.querySelector(".game-screen");
+const startScreen = document.querySelector(".start-screen");
+
+const modal = document.querySelector(".modal-overlay");
+
+readRulesButton.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+modal.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target !== document.querySelector(".modal-container")) {
+    modal.style.display = "none";
+  }
+});
+
+startButton.addEventListener("click", () => {
+  startScreen.style.display = "none";
+  gameScreen.style.display = "block";
+});
 
 let player;
 let computer;
@@ -14,81 +39,75 @@ let playerScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 /* For Each loop that runs through player and computer choice and calls on functions*/
-choicebutton.forEach(button => button.addEventListener('click', () => {
+choiceButton.forEach((button) =>
+  button.addEventListener("click", () => {
     player = button.id;
-    computersGo(computerChoice => {
-        userChoice.textContent = `Player: ${player}`;
-        computerChoiceElement.textContent = `Computer: ${computerChoice}`;
-        winnerIs.textContent = checkwinner();
-        updateScoreboard()
-        if (playerScore === 3 || computerScore === 3) {
-            reset()
-          }
-        });
-      }));
- 
-/* function for computer to choose a random choice */
+    computersGo((computerChoice) => {
+      userChoice.textContent = `Player: ${player}`;
+      computerChoiceElement.textContent = `Computer: ${computerChoice}`;
+      winnerIs.textContent = checkWinner();
+      updateScoreboard();
+      if (playerScore === 3 || computerScore === 3) {
+        reset();
+      }
+    });
+  })
+);
+
+/** function for computer to choose a random choice */
 function computersGo(callback) {
-    const random = Math.floor(Math.random() * 3) + 1;
-    let computerChoiceText;
+  const random = Math.floor(Math.random() * 3) + 1;
+  let computerChoiceText;
 
-    switch (random) {
-        case 1:
-            computerChoiceText = 'Rock';
-            break;
-        case 2:
-            computerChoiceText = 'Paper';
-            break;
-        case 3:
-            computerChoiceText = 'Scissors';
-            break;
-    }
+  switch (random) {
+    case 1:
+      computerChoiceText = "Rock";
+      break;
+    case 2:
+      computerChoiceText = "Paper";
+      break;
+    case 3:
+      computerChoiceText = "Scissors";
+      break;
+  }
 
-    computer = computerChoiceText;
-    callback(computerChoiceText);
+  computer = computerChoiceText;
+  callback(computerChoiceText);
 }
-/* function that checks user choice and checks computer choice and compares */
-function checkwinner() {
-    if (player === computer) {
-        drawScore++;
-        return "Draw!";
-    } else if (
-        (player === 'Rock' && computer === 'Scissors') ||
-        (player === 'Paper' && computer === 'Rock') ||
-        (player === 'Scissors' && computer === 'Paper')
-    ) {
-        playerScore++;
-        return 'You Win!';
-        
-    } else {
-        computerScore++;
-        return 'You Lose!';
-
-    }
-    
+/** function that checks user choice and checks computer choice and compares */
+function checkWinner() {
+  if (player === computer) {
+    drawScore++;
+    return "Draw!";
+  } else if (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Paper" && computer === "Rock") ||
+    (player === "Scissors" && computer === "Paper")
+  ) {
+    playerScore++;
+    return "You Win!";
+  } else {
+    computerScore++;
+    return "You Lose!";
+  }
 }
 /* function for uodating scoreboard and resetting when winner reaches 3*/
 function updateScoreboard() {
-    playerScoreElement.textContent = playerScore;
-    computerScoreElement.textContent = computerScore;
-    drawScoreElement.textContent = drawScore;
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+  drawScoreElement.textContent = drawScore;
 
   if (playerScore === 3) {
-    winnerIs.textContent = "Congratulations! You win! Make a choice to play again";
+    winnerIs.textContent =
+      "Congratulations! You win! Make a choice to play again";
   } else if (computerScore === 3) {
-    winnerIs.textContent = "You've lost the game. Unlucky! Make a choice to play again";
+    winnerIs.textContent =
+      "You've lost the game. Unlucky! Make a choice to play again";
   }
-
 }
-  /* function to reset scores to zero */
-  function reset (){
-    playerScore = 0;
-    computerScore = 0;
-    drawScore = 0;
-  }
-
-  function showAlert() {
-    var myText = "This is a game of Rock Paper Scissors designed for my Project 2 with the Code Institue, In this game you will play against the computer in a First to Three game. Press Ok and make a choice to begin. Enjoy!";
-    alert (myText);
-  }
- 
+/* function to reset scores to zero */
+function reset() {
+  playerScore = 0;
+  computerScore = 0;
+  drawScore = 0;
+}
